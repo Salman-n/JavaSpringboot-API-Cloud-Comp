97114d2b2d63dd56a0e5cb58ffa3fcb4626f63ad
Salman W <71618052+Salman-n@users.noreply.github.com>
2024-01-02 20:50:32 +0700

    Add files via upload

---
+++ UASCC_K3520070/src/test/java/com/salman/eurekaserver/UasccK3520070ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salman.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UasccK3520070ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
